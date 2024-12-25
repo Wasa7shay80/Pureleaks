89169c6e591f4a7babe5c46384783592a3aa95b7
Wasa7shay80 <158795096+Wasa7shay80@users.noreply.github.com>
2024-12-26 04:48:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Drink")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002467223104"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002349729099"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002389283433"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
