e9eadb95a0d9c9e7b3f70182a512a9d748b4e54b
Wasa7shay80 <158795096+Wasa7shay80@users.noreply.github.com>
2025-03-26 11:14:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Snappyy80")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002258063489"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", " -1002259254411"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002467223104"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
